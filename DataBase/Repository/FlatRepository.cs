0bc7b463c6840e214a18621ccb84e7ef6024c65e
Timofey Kuznetsov <kuznetsov.timofey.2003@mail.ru>
2024-06-03 23:12:44 +1000

    finish flat repo

--- DataBase/Repository/FlatRepository.cs
+++ DataBase/Repository/FlatRepository.cs
@@ -19,7 +19,7 @@ public class FlatRepository : IFlatRepository
         command.Parameters.AddWithValue("highPrice", highPrice);
         command.Parameters.AddWithValue("lowArea", lowArea);
         command.Parameters.AddWithValue("highArea", highArea);
-        using var reader = command.ExecuteReader();
+        await using var reader = await command.ExecuteReaderAsync();
         List<Flat> flats = new List<Flat>();
         if (reader.HasRows)
         {
@@ -49,7 +49,7 @@ public class FlatRepository : IFlatRepository
         var stringCommand = "SELECT * FROM flats WHERE windows_world_side = @worldSIde";
         using var command = new NpgsqlCommand(stringCommand, _context.connection);
         command.Parameters.AddWithValue("worldSide", worldSide);
-        using var reader =  command.ExecuteReader();
+        await using var reader = await command.ExecuteReaderAsync();
         List<Flat> flats = new List<Flat>();
         if (reader.HasRows)
         {
@@ -76,19 +76,45 @@ public class FlatRepository : IFlatRepository
 
     public async Task<IEnumerable<Flat>> GetFlatsForYoungFamile(List<InfrastructureObject> infObjects)
     {
+        string objectList = "";
+        foreach (var infObj in infObjects)
+        {
+            objectList += "'" + infObj.Name + "',";
+        }
+        objectList.Remove(objectList.Length - 1);
+
         string stringCommand = "SELECT *" +
                                 "FROM (" +
                                     "SELECT rcio.resedential_complex_id, rcio.infrastructure_object_id" +
                                     "FROM resedential_complexes_infrastructure_objects as rcio" +
                                     "INNER JOIN infrastructure_objects as io" +
                                     "ON rcio.infrastructure_object_id = io.id" +
-                                    "WHERE io.name IN ('Sport', 'Medecine', 'Education')" +
-                                ") as result" +
+                                    "WHERE io.name IN (" + objectList + ") +) as result" +
                                 "INNER JOIN flats" +
                                 "ON flats.resedential_complex_id = result.resedential_complex_id";
-        foreach (var infObject in infObjects)
+        using var command = new NpgsqlCommand(stringCommand, _context.connection);
+        await using var reader = await command.ExecuteReaderAsync();
+        List<Flat> flats = new List<Flat>();
+        if (reader.HasRows)
         {
-            
+            while (await reader.ReadAsync())
+            {
+                var newFlat = new Flat() {
+                    Id = reader.GetInt32(0),
+                    ResidentialComplexId = reader.GetInt32(1),
+                    Price = reader.GetInt32(2),
+                    Area = reader.GetDouble(3),
+                    RoomQuantity = reader.GetInt32(4),
+                    FloorNumber = reader.GetInt32(5),
+                    WorldSide = (WorldSide)reader.GetInt32(6),
+                    Status = (BuildingStatus)reader.GetInt32(7),
+                    IsMortgageAvailable = reader.GetBoolean(8),
+                    IsSold = reader.GetBoolean(9)
+                };
+                flats.Add(newFlat);
+            }
+            return flats;
         }
+        return flats;
     }
 }
