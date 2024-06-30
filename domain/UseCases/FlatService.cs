943e78351d4941d3430e80229bd76c0f72410ec7
Timofey Kuznetsov <kuznetsov.timofey.2003@mail.ru>
2024-06-30 13:58:20 +1000

    add validation for flat service

--- domain/UseCases/FlatService.cs
+++ domain/UseCases/FlatService.cs
@@ -13,21 +13,36 @@ public class FlatService
 
     public Result<List<Flat>> GetFlatsByPriceAndArea(int lowPrice, int highPrice, double lowArea, double highArea)
     {
+        if (lowPrice < 0 || highPrice < lowPrice)
+        {
+            return Result.Fail<List<Flat>>("Invalid price");
+        }
+
+        if (lowArea <= 0 || highArea < lowArea)
+        {
+            return Result.Fail<List<Flat>>("Invalid area");
+        }
+
         var flats =  _db.GetFlatsByPriceAndArea(lowPrice, highPrice, lowArea, highArea);
-        return Result.Ok(flats);
+        
+        return flats != null ? Result.Ok(flats) : Result.Fail<List<Flat>>("Flats not found");
     }
 
     public Result<List<Flat>> GetFlatsByWindowsSide(WorldSide worldSide)
     {
+        if (!Enum.IsDefined(typeof(WorldSide), worldSide))
+            return Result.Fail<List<Flat>>("Unknown enum type"); //maybe useless #Fixme         
         var mapWorldSide = Convert.ToInt32(worldSide);
         var flats = _db.GetFlatsByWindowWorldeSide(mapWorldSide);
-        return Result.Ok(flats); 
+        return flats != null ? Result.Ok(flats) : Result.Fail<List<Flat>>("Flats not found"); 
     }
 
     public Result<List<Flat>> GetFlatsForYoungFamily(List<InfrastructureObject> objects)
     {
+        if (objects == null)
+            return Result.Fail<List<Flat>>("Empty inf objects list");
         var flats = _db.GetFlatsForYoungFamily(objects);
-        return Result.Ok(flats);
+        return flats != null ? Result.Ok(flats) : Result.Fail<List<Flat>>("Flats not found"); 
         
     }
     
